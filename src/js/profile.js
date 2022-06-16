c3b05614cd95302307029f726329f6182c3d7ddd
krrlclfr <kororolucilfer@gmail.com>
2022-06-16 10:23:03 +0800

    Archer

--- src/js/profile.js
+++ src/js/profile.js
@@ -2,13 +2,32 @@ $(document).ready(function () {
 
     $('#frontend').click(function(){
         $('#frontend-list').slideToggle();
+        $('#frameworks-list').fadeOut();
+        $('#mobile-list').fadeOut();
         $('#backend-list').fadeOut();
         $('#database-list').fadeOut();
         
+        
+    })
+    $('#frameworks').click(function(){
+        $('#frameworks-list').slideToggle();
+        $('#frontend-list').fadeOut();
+        $('#mobile-list').fadeOut();
+        $('#backend-list').fadeOut();
+        $('#database-list').fadeOut();
+    })
+    $('#mobile-app').click(function(){
+        $('#mobile-list').slideToggle();
+        $('#frontend-list').fadeOut();
+        $('#frameworks-list').fadeOut();
+        $('#backend-list').fadeOut();
+        $('#database-list').fadeOut();
     })
 
     $('#backend').click(function(){
         $('#backend-list').slideToggle();
+        $('#frameworks').fadeOut();
+        $('#mobile-list').fadeOut();
         $('#frontend-list').fadeOut();
         $('#database-list').fadeOut();
 
@@ -16,6 +35,8 @@ $(document).ready(function () {
     })
     $('#database').click(function(){
         $('#database-list').slideToggle();
+        $('#frameworks-list').fadeOut();
+        $('#mobile-list').fadeOut();
         $('#backtend-list').fadeOut();
         $('#frontend-list').fadeOut();
 
